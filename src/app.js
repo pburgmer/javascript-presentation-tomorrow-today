cf69b0676ac18f3cdcec5c5555da2b18281470c9
Philipp Burgmer <burgmer@weiglewilczek.com>
2015-06-18 00:37:05 +0200

    move 'modules' after 'promises'

--- src/app.js
+++ src/app.js
@@ -35,18 +35,18 @@ angular.module('app').config (function (slidesConfig) {
     'features/promises/q',
     'features/promises/es6',
 
+    'features/modules/overview',
+    'features/modules/problems',
+    'features/modules/es6-angular',
+    'features/modules/es6-angular-on-top',
+    'features/modules/es6-angular-instead-of',
+
     'features/classes/overview',
     'features/classes/angular-services',
     'features/classes/angular-services-with-classes',
 
     'features/arrow-functions/overview',
     'features/arrow-functions/usecases',
-
-    'features/modules/overview',
-    'features/modules/problems',
-    'features/modules/es6-angular',
-    'features/modules/es6-angular-on-top',
-    'features/modules/es6-angular-instead-of',
 
     'further-reading',
 
